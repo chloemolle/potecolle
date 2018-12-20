517ac129924e767c7d3a1c6adbb1c8c25ab600d2
chloemolle <chloe.molle@telecom-paristech.fr>
2018-12-20 12:56:36 +0100

    clean connexion page

--- app/src/main/java/com/example/chloemolle/potecolle/ConfigureComptePage.java
+++ app/src/main/java/com/example/chloemolle/potecolle/ConfigureComptePage.java
@@ -58,12 +58,9 @@ public class ConfigureComptePage extends Activity {
         final ProgressBar progressBarCheckUsername = (ProgressBar) findViewById(R.id.progress_bar_check_username_configuration);
 
         progressBarCheckUsername.setVisibility(View.GONE);
-
         final Globals globalVariables = (Globals) getApplicationContext();
-
-        EditText text = (EditText) findViewById(R.id.username_text);
         final String previousUserName = globalVariables.getUser().getUsername();
-        text.setText(previousUserName);
+        setUserInformation(previousUserName);
 
 
         Button backButton = (Button) findViewById(R.id.retour_main_page_from_configuration);
@@ -75,133 +72,21 @@ public class ConfigureComptePage extends Activity {
             }
         });
 
-        final Spinner choixClasse = (Spinner) findViewById(R.id.spinner_classe_update);
-        ArrayAdapter<CharSequence> adapterFamily = ArrayAdapter.createFromResource(this,
-                R.array.classes, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapterFamily.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        choixClasse.setAdapter(adapterFamily);
-        int spinnerPosition = adapterFamily.getPosition(globalVariables.getUser().getClasse());
-        //set the default according to value
-        choixClasse.setSelection(spinnerPosition);
+        //Bouton pour supprimer le compte
+        supprimerCompte(previousUserName);
 
-        Button supprimerCompte = (Button) findViewById(R.id.supprimer_compte);
-        supprimerCompte.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View v) {
-                AlertDialog.Builder alertDialog = new AlertDialog.Builder(v.getContext());
-                alertDialog.setPositiveButton(R.string.supprimer, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
+        //Bouton pour modifier le compte
+        updateCompte(progressBarCheckUsername, previousUserName);
 
+    }
 
-                        for (final Game partie: globalVariables.getUser().getPartiesEnCours()) {
-                            userDB.collection("Games")
-                                    .document(partie.getId())
-                                    .delete()
-                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<Void> task) {
-                                            Log.d("réussi", "yeayyy");
-                                        }
-                                    })
-                                    .addOnFailureListener(new OnFailureListener() {
-                                        @Override
-                                        public void onFailure(@NonNull Exception e) {
-                                            Log.d("fail", e.getMessage());
-                                        }
-                                    });
-                        }
-
-
-                        for (final FriendRequest friendRequest: globalVariables.getUser().getFriendRequests()) {
-                            userDB.collection("FriendRequests")
-                                    .document(friendRequest.getId())
-                                    .delete()
-                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<Void> task) {
-                                            db.collection("Users").document(friendRequest.getId())
-                                                    .collection("friendRequests")
-                                                    .document(userFirebase.getEmail())
-                                                    .delete()
-                                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                                        @Override
-                                                        public void onComplete(@NonNull Task<Void> task) {
-                                                            Log.d("reussi", "yes");
-                                                        }
-                                                    })
-                                                    .addOnFailureListener(new OnFailureListener() {
-                                                        @Override
-                                                        public void onFailure(@NonNull Exception e) {
-                                                            Log.d("fail", e.getMessage());
-                                                        }
-                                                    });
-                                            Log.d("réussi", "yeayyy");
-                                        }
-                                    })
-                                    .addOnFailureListener(new OnFailureListener() {
-                                        @Override
-                                        public void onFailure(@NonNull Exception e) {
-                                            Log.d("fail", e.getMessage());
-                                        }
-                                    });
-                        }
-
-
-
-                        userDB.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                Log.d("Reussi", "User deleted");
-
-
-                                userFirebase.delete()
-                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                            @Override
-                                            public void onComplete(@NonNull Task<Void> task) {
-                                                Log.d("Reussi", "User deleted");
-
-                                                Intent intent = new Intent(v.getContext(), ConnexionPage.class);
-                                                startActivity(intent);
-                                            }
-                                        })
-                                        .addOnFailureListener(new OnFailureListener() {
-                                            @Override
-                                            public void onFailure(@NonNull Exception e) {
-                                                Log.d("Fail", "User NOT deleted");
-                                            }
-                                        });
-
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                    Log.d("Fail", "User NOT deleted");
-                                }
-                            });
-                        }
-                });
-
-                alertDialog.setNegativeButton(R.string.annuler, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                    }
-                });
-
-                alertDialog.setTitle("Suppression compte");
-                alertDialog.setMessage(R.string.supprimer_compte_question);
-                alertDialog.setCancelable(true);
-                alertDialog.create().show();
-            }
-        });
-
+    public void updateCompte(final ProgressBar progressBarCheckUsername, final String previousUserName) {
         Button update = (Button) findViewById(R.id.update);
         update.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final Spinner choixClasse = (Spinner) findViewById(R.id.spinner_classe_update);
-
+                final Globals globalVariables = (Globals) getApplicationContext();
                 final EditText text = (EditText) findViewById(R.id.username_text);
                 final String userClass = choixClasse.getSelectedItem().toString();
                 final FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -219,7 +104,6 @@ public class ConfigureComptePage extends Activity {
                             @Override
                             public String then(@NonNull Task<HttpsCallableResult> task) throws Exception {
                                 final ArrayList<HashMap<String, String>> arr = (ArrayList<HashMap<String, String>>) task.getResult().getData();
-                                final ArrayList<Integer> indexesToRemove = new ArrayList<>();
                                 Boolean alreadyUsed = false;
                                 String username = text.getText().toString();
                                 for (final HashMap<String, String> ami : arr) {
@@ -247,13 +131,8 @@ public class ConfigureComptePage extends Activity {
                                                     View layout = inflater.inflate(R.layout.toast,
                                                             (ViewGroup) findViewById(R.id.custom_toast_container));
 
-                                                    TextView textToast = (TextView) layout.findViewById(R.id.text);
-                                                    textToast.setText("Nous avons bien pris en compte vos changements ! :)");
-                                                    Toast toast = new Toast(getApplicationContext());
-                                                    toast.setGravity(Gravity.BOTTOM, 0, 0);
-                                                    toast.setDuration(Toast.LENGTH_LONG);
-                                                    toast.setView(layout);
-                                                    toast.show();
+                                                    Globals.makeToast("Nous avons bien pris en compte vos changements ! :)", layout,  context);
+
                                                     progressBarCheckUsername.setVisibility(View.GONE);
 
                                                     Intent intent = new Intent(context, MainPage.class);
@@ -291,5 +170,158 @@ public class ConfigureComptePage extends Activity {
 
     }
 
+    public void setUserInformation(String previousUserName) {
+        final Globals globalVariables = (Globals) getApplicationContext();
+        EditText text = (EditText) findViewById(R.id.username_text);
+        text.setText(previousUserName);
+
+        final Spinner choixClasse = (Spinner) findViewById(R.id.spinner_classe_update);
+        ArrayAdapter<CharSequence> adapterFamily = ArrayAdapter.createFromResource(this,
+                R.array.classes, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapterFamily.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        choixClasse.setAdapter(adapterFamily);
+        int spinnerPosition = adapterFamily.getPosition(globalVariables.getUser().getClasse());
+        //set the default according to value
+        choixClasse.setSelection(spinnerPosition);
+    }
+
+    public void supprimerCompte(String previousUserName) {
+        final FirebaseUser userFirebase = FirebaseAuth.getInstance().getCurrentUser();
+        final String userEmail = userFirebase.getEmail();
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+        final DocumentReference userDB = db.collection("Users").document(userEmail);
+
+        Button supprimerCompte = (Button) findViewById(R.id.supprimer_compte);
+        supprimerCompte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(v.getContext());
+                alertDialog.setPositiveButton(R.string.supprimer, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        suppressionParties(userDB);
+
+                        suppressionFriendRequest();
+
+                        suppressionUser(userDB, userFirebase, v.getContext());
+
+                    }
+                });
+
+                alertDialog.setNegativeButton(R.string.annuler, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                });
+
+                alertDialog.setTitle("Suppression compte");
+                alertDialog.setMessage(R.string.supprimer_compte_question);
+                alertDialog.setCancelable(true);
+                alertDialog.create().show();
+            }
+        });
+
+    }
+
+    public void suppressionParties(DocumentReference userDB) {
+        final Globals globalVariables = (Globals) getApplicationContext();
+
+        for (final Game partie: globalVariables.getUser().getPartiesEnCours()) {
+            userDB.collection("Games")
+                    .document(partie.getId())
+                    .delete()
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            Log.d("réussi", "C'est bon");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d("fail", e.getMessage());
+                        }
+                    });
+        }
+
+    }
+
+    public void suppressionFriendRequest() {
+        final FirebaseUser userFirebase = FirebaseAuth.getInstance().getCurrentUser();
+        final String userEmail = userFirebase.getEmail();
+
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+        final DocumentReference userDB = db.collection("Users").document(userEmail);
+        final Globals globalVariables = (Globals) getApplicationContext();
+
+        for (final FriendRequest friendRequest: globalVariables.getUser().getFriendRequests()) {
+            userDB.collection("FriendRequests")
+                    .document(friendRequest.getId())
+                    .delete()
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            db.collection("Users").document(friendRequest.getId())
+                                    .collection("friendRequests")
+                                    .document(userFirebase.getEmail())
+                                    .delete()
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            Log.d("reussi", "yes");
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Log.d("fail", e.getMessage());
+                                        }
+                                    });
+                            Log.d("réussi", "yeayyy");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d("fail", e.getMessage());
+                        }
+                    });
+        }
+
+    }
+
+    public void suppressionUser(DocumentReference userDB, final FirebaseUser userFirebase, final Context context) {
+        userDB.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                Log.d("Reussi", "User deleted");
+
+
+                userFirebase.delete()
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Log.d("Reussi", "User deleted");
+
+                                Intent intent = new Intent(context, ConnexionPage.class);
+                                startActivity(intent);
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.d("Fail", "User NOT deleted");
+                            }
+                        });
+
+            }
+        })
+        .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("Fail", "User NOT deleted");
+            }
+        });
+    }
 
 }
